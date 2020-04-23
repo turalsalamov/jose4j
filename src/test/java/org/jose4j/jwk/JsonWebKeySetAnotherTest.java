5b5fde5512e48296fc65300dcf3043a98702cf6b
Brian Campbell <brian.d.campbell@gmail.com>
2020-04-23 15:27:06 -0600

    add a test with a JWK having an unknown type in a JWKS to ensure that it is basically ignored and the other JWKs are still processed

--- src/test/java/org/jose4j/jwk/JsonWebKeySetAnotherTest.java
+++ src/test/java/org/jose4j/jwk/JsonWebKeySetAnotherTest.java
@@ -51,4 +51,44 @@ public class JsonWebKeySetAnotherTest
         JsonWebKeySet jwks = new JsonWebKeySet(json);
         Assert.assertThat(3, equalTo(jwks.getJsonWebKeys().size()));
     }
+
+    @Test
+    public void oneUnknownKTY() throws JoseException
+    {
+        // one of them is an unknown kty
+        String json =
+                "{" +
+                "  \"keys\": [" +
+                "    {" +
+                "      \"kty\": \"EC\"," +
+                "      \"kid\": \"96\"," +
+                "      \"x\": \"bfOKLR8w_vD7ce9o_hmxfqTcNo9joJIALo4xC_-Qhzg\"," +
+                "      \"y\": \"y2jXZtCaeoLGQIiJx5-kHLT3SlP7nzQbnP8SLUl1vg4\"," +
+                "      \"crv\": \"P-256\"" +
+                "    }," +
+                "    {" +
+                "      \"kty\": \"EC\"," +
+                "      \"kid\": \"a9\"," +
+                "      \"x\": \"msdBj_jUyuw_qCkNXTGjGpibVc_FE5FaexmE_qTWKmY\"," +
+                "      \"y\": \"lDHAX8xJ17zRDtPcPzQmFurVtOJllmOK2jPwCGZ57TQ\"," +
+                "      \"crv\": \"P-256\"" +
+                "    }," +
+                "   {" +
+                "     \"kty\":\"UNKNOWN\"," +
+                "     \"crv\":\"whatever\","
+                        +
+                "     \"x\":\"11qYAYKxCrfVS_7TyWQHOg7hcvPapiMlrwIaaPcHURo\"" +
+                "   }" +
+                "    {" +
+                "      \"kty\": \"EC\"," +
+                "      \"kid\": \"2d\"," +
+                "      \"x\": \"l3V6TH8tuS0vWSpZ9KcUW4oDuBzOTN0v2C_dsqkrHKw\"," +
+                "      \"y\": \"Yhg6pR__nALI6sp68NcQM6FlPaod83xUXgHKGOCJHJ4\"," +
+                "      \"crv\": \"P-256\"" +
+                "    }" +
+                "  ]" +
+                "}";
+        JsonWebKeySet jwks = new JsonWebKeySet(json);
+        Assert.assertThat(3, equalTo(jwks.getJsonWebKeys().size()));
+    }
 }
